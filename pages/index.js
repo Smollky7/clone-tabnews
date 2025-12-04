5a32e6effe4361ef4b0cdbd23901c29d3dd060a8
Smollky7 <jarbas81@gmail.com>
2025-12-04 14:46:28 -0300

    alteracao da mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-  return <h1>Amo, eu amo voce, se voce me ama tambem, me manda uma fotozinha!</h1>
+  return <h1>Amo, eu amo voce, se voce me ama tambem, me manda uma fotozinha! ❤️</h1>
 }
 export default Home;
