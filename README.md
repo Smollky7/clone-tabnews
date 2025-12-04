fd367d257c0d99c862f3af8c2ee76a61d5811186
Smollky7 <jarbas81@gmail.com>
2025-12-04 17:05:13 -0300

    adicionando o `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br para 0 https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 function Home() {
-  return <h1>Ash meu amo, eu amo você, se você me ama também, me manda uma fotozinha e um joinha! ❤️</h1>
+  return (
+    <h1>
+      Ash meu amo, eu amo você, se você me ama também, me manda uma fotozinha e
+      um joinha! ❤️
+    </h1>
+  );
 }
-export default Home; 
+
+export default Home;
