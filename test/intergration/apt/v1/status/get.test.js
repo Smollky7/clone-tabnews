538c6148a7b7834135add67f31177c5c7eef594b
Smollky7 <jarbas81@gmail.com>
2025-12-21 18:23:02 -0300

    finaliza o endpind de

--- test/intergration/apt/v1/status/get.test.js
+++ test/intergration/apt/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status shoule return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBoby = await response.json();
+
+  const parserUpdateAt = new Date(responseBoby.updated_at).toISOString();
+  expect(responseBoby.updated_at).toEqual(parserUpdateAt);
+
+  expect(responseBoby.dependecies.database.version).toEqual("16.0");
+  expect(responseBoby.dependecies.database.max_connections).toEqual(100);
+  expect(responseBoby.dependecies.database.opened_connections).toEqual(1);
 });
